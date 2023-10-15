a46e842fa476d1fe737005410980d3e179c11b1f
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-15 20:36:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts';
 import { createInterface } from 'readline'
 import yargs from 'yargs'
+import keep_alive from './keep_alive.js'
 
 // https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -16,12 +17,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say('ABHISHEK SER', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @ABHISHEK-SURESH._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
@@ -61,7 +62,7 @@ function start(file) {
   })
   p.on('exit', (_, code) => {
     isRunning = false
-    console.error('❎ An Unexpected Error Occurred:', code)
+    console.error('❌ An Unexpected Error Occurred:', code)
     if (code === 0) return
     watchFile(args[0], () => {
       unwatchFile(args[0])
@@ -77,3 +78,4 @@ function start(file) {
 }
 
 start('main.js')
+keep_alive();
