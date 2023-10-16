b2bedf3106e7f91ab5ba58b8b92021df243eb59e
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-17 00:58:35 +0530

    Update gp-add.js

--- plugins/gp-add.js
+++ plugins/gp-add.js
@@ -4,9 +4,9 @@ recomiendo no usar en wa mod
 
 import fetch from 'node-fetch'
 /**
- * @type {import('@adiwajshing/baileys')}
+ * @type {import('@whiskeysockets/baileys')}
  */
-const { getBinaryNodeChild, getBinaryNodeChildren } = (await import('@adiwajshing/baileys')).default
+const { getBinaryNodeChild, getBinaryNodeChildren } = (await import('@whiskeysockets/baileys')).default
 let handler = async (m, { conn, text, participants }) => {
     let _participants = participants.map(user => user.id)
     let users = (await Promise.all(
@@ -40,7 +40,7 @@ let handler = async (m, { conn, text, participants }) => {
         const content = getBinaryNodeChild(user, 'add_request')
         const invite_code = content.attrs.code
         const invite_code_exp = content.attrs.expiration
-        let teks = `✳️ Al usuario @${jid.split('@')[0]} solo lo pueden agregar sus contactos :'v `
+        let teks = `🎯 To The User @${jid.split('@')[0]} You Can Only Add Your Contacts :'v `
         m.reply(teks, null, {
             mentions: conn.parseMention(teks)
         })
@@ -56,5 +56,3 @@ handler.rowner = true
 handler.botAdmin = true
 
 export default handler
-
-
