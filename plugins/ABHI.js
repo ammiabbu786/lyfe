2573d9990b3ea68b010d4c279e923595b3aae6ee
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-17 02:27:12 +0530

    Update ABHI.js

--- plugins/ABHI.js
+++ plugins/ABHI.js
@@ -5,17 +5,17 @@ let handler = async (m, { conn, text }) => {
     } else {
         who = m.chat;
     }
-    if (!who) throw 'Tag the person you want to make an Owner!';
+    if (!who) throw '*🎯Tag A Person*';
     if (global.owner.includes(who.split('@')[0])) throw 'This person is already an owner!';
     global.owner.push([who.split('@')[0], m.name, true]);
-    const caption = `Now @${who.split('@')[0]} has been made an Owner!`;
+    const caption = `*Now @${who.split('@')[0]} Got Acess To Owner Only Commands✅*`;
     await conn.reply(m.chat, caption, m, {
         mentions: conn.parseMention(caption)
     });
 }
-handler.help = ['addowner @user']
+handler.help = ['sudo @user']
 handler.tags = ['owner']
-handler.command = /^(add|give|-)(owner|sudo)$/i;
+handler.command = /^(owner|sudo)$/i;
 handler.owner = true
 
 export default handler;
