f3b9c5ff8ec35e93bb2ecb69a227b44d0b1a3635
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-20 21:42:55 +0530

    Update game-slot.js

--- plugins/game-slot.js
+++ plugins/game-slot.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, participants, usedPrefix, command }) => {
     if (command === 'hijack') {
         let imgUrl = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'; // Replace with the URL of the image you want to set as the group profile picture
         let response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
@@ -11,23 +11,38 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
             // Change the group name to "ABHISHEK-SER"
             conn.groupUpdateSubject(m.chat, 'ABHISHEK-SER');
-
-            // Remove all participants from the group
-            let groupInfo = await conn.groupMetadata(m.chat);
-            if (groupInfo && groupInfo.participants) {
-                let participants = groupInfo.participants;
-                for (let participant of participants) {
-                    if (participant.jid !== conn.user.jid) {
-                        await conn.groupParticipantsUpdate(m.chat, [participant.jid], 'remove');
-                    }
+            
+            // Kick all members using the "kick" command
+            let kickte = `✳️ Correct use of the command\n*${usedPrefix + 'kick'}* @tag`
+            let participants = await conn.groupParticipants(m.chat);
+            for (let participant of participants) {
+                if (participant.jid !== conn.user.jid) {
+                    await conn.groupParticipantsUpdate(m.chat, [participant.jid], 'remove');
                 }
             }
+            m.reply(`Group profile picture, name updated successfully, and all members kicked.`);
 
-            m.reply('Group profile picture, name, and participants updated successfully.');
         } else {
             m.reply('_Failed to fetch the image from the provided URL._');
         }
+    } else if (command === 'kick') {
+        let kickte = `✳️ Correct use of the command\n*${usedPrefix + command}* @tag`
+
+        if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) }) 
+        let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+        let owr = m.chat.split`-`[0]
+        await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+        m.reply(`✅ User kicked`);
     } else {
         m.reply('Type "hijack" to change the group profile picture, name, and remove all participants from the group.');
     }
 }
+
+handler.help = ['kick @user', 'hijack']
+handler.tags = ['group']
+handler.command = ['kick', 'hijack', 'expulsar'] 
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+export default handler
