ad192bd45514e369d7a14086abf070660805383d
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-20 10:04:24 +0530

    Update owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,39 +1,40 @@
-import cp, { exec as _exec } from 'child_process'
-import { promisify } from 'util'
+import cp, { exec as _exec } from 'child_process';
+import { promisify } from 'util';
 
-let exec = promisify(_exec).bind(cp)
+const exec = promisify(_exec).bind(cp);
 
-let handler = async (m, { conn, isOwner, command, text }) => {
-  if (conn.user.jid != conn.user.jid) return
+const handler = async (m, { conn, isOwner, text }) => {
+  if (conn.user.jid !== conn.user.jid) return;
 
-  if (text.startsWith('>return m.quoted')) {
-    // Extract the quoted message
-    const quotedMessage = m.quoted
-
-    // You can process the quoted message here or customize your action
-    // For example, send a specific reply:
-    m.reply(`You requested to return the quoted message: \n${JSON.stringify(quotedMessage, null, 2)}`)
-    
+  // Check if the message starts with ">eval"
+  if (text.startsWith('>eval')) {
+    const codeToEvaluate = text.replace('>eval', '').trim(); // Extract the code to evaluate
+    try {
+      const result = eval(codeToEvaluate); // Evaluate the provided code
+      m.reply(`Result of evaluation: ${result}`);
+    } catch (error) {
+      m.reply(`Error during evaluation: ${error}`);
+    }
   } else {
     // Handle other commands or execute shell commands
-    m.reply('✅ running...')
-    let o
+    m.reply('✅ running...');
+    let o;
     try {
-      o = await exec(command.trimStart()  + ' ' + text.trimEnd())
+      o = await exec(text.trim());
     } catch (e) {
-      o = e
+      o = e;
     } finally {
-      let { stdout, stderr } = o
-      if (stdout.trim()) m.reply(stdout)
-      if (stderr.trim()) m.reply(stderr)
+      const { stdout, stderr } = o;
+      if (stdout.trim()) m.reply(stdout);
+      if (stderr.trim()) m.reply(stderr);
     }
   }
-}
+};
 
-handler.help = ['>']
-handler.tags = ['advanced']
-handler.customPrefix = /^[>] /
-handler.command = new RegExp
-handler.rowner = true
+handler.help = ['>eval'];
+handler.tags = ['advanced'];
+handler.customPrefix = /^[>] /;
+handler.command = new RegExp;
+handler.rowner = true;
 
-export default handler
+export default handler;
