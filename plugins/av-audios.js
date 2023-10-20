26178ef26b6e958475646b991da4cd1b1f9bfa09
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-20 11:08:58 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -1,24 +1,33 @@
+let bgmMap = new Map(); // Map to store the associated BGM with text
+
 let handler = m => m
 handler.all = async function (m) {
+  if (/^\.setbgm/i.test(m.text)) {
+    // Check if the message contains a quoted audio file
+    if (m.quoted && m.quoted.mimetype && m.quoted.mimetype.startsWith('audio/')) {
+      const text = m.text.split(' ').slice(1).join(' '); // Extract the associated text
+      const audioUrl = await this.downloadMediaMessage(m.quoted); // Get the URL of the audio
 
-    if (/^.alive$/i.test(m.text) ) {
-      let av = './src/mp3/Abhi.mp3'
-      this.sendPresenceUpdate('recording', m.chat);
-      this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-      }
+      // Store the BGM and associated text in the map
+      bgmMap.set(text, audioUrl);
 
-  if (/^.menu$/i.test(m.text) ) {
-    let av = './src/mp3/menu.mp3'
-    this.sendPresenceUpdate('recording', m.chat);
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-   }
+      this.reply(m.chat, `Background audio set for: "${text}"`, m);
+    }
+  }
 
-  if (/^bgm$/i.test(m.text) ) {
-    let av = './src/mp3/bgm.mp3'
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-   }
-  
-return !0
- }
- 
-export default handler
+  if (/^.bgm/i.test(m.text)) {
+    const text = m.text.split(' ').slice(1).join(' ');
+    const audioUrl = bgmMap.get(text);
+
+    if (audioUrl) {
+      // Send the associated BGM
+      this.sendFile(m.chat, audioUrl, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
+    } else {
+      this.reply(m.chat, `Background audio not found for: "${text}"`, m);
+    }
+  }
+
+  return !0;
+}
+
+export default handler;
