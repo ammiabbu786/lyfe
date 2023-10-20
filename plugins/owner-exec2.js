4323b5a32beb119a6268f9bee1881042ae287211
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-20 09:52:07 +0530

    Update owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,23 +1,39 @@
 import cp, { exec as _exec } from 'child_process'
 import { promisify } from 'util'
+
 let exec = promisify(_exec).bind(cp)
+
 let handler = async (m, { conn, isOwner, command, text }) => {
   if (conn.user.jid != conn.user.jid) return
-  m.reply('✅ running...')
-  let o
-  try {
-    o = await exec(command.trimStart()  + ' ' + text.trimEnd())
-  } catch (e) {
-    o = e
-  } finally {
-    let { stdout, stderr } = o
-    if (stdout.trim()) m.reply(stdout)
-    if (stderr.trim()) m.reply(stderr)
+
+  if (text.startsWith('>return m.quoted')) {
+    // Extract the quoted message
+    const quotedMessage = m.quoted
+
+    // You can process the quoted message here or customize your action
+    // For example, send a specific reply:
+    m.reply(`You requested to return the quoted message: \n${JSON.stringify(quotedMessage, null, 2)}`)
+    
+  } else {
+    // Handle other commands or execute shell commands
+    m.reply('✅ running...')
+    let o
+    try {
+      o = await exec(command.trimStart()  + ' ' + text.trimEnd())
+    } catch (e) {
+      o = e
+    } finally {
+      let { stdout, stderr } = o
+      if (stdout.trim()) m.reply(stdout)
+      if (stderr.trim()) m.reply(stderr)
+    }
   }
 }
-handler.help = ['$']
+
+handler.help = ['>']
 handler.tags = ['advanced']
-handler.customPrefix = /^[$] /
+handler.customPrefix = /^[>] /
 handler.command = new RegExp
 handler.rowner = true
+
 export default handler
