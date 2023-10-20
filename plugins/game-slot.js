7e8219cf88f52f5d693cb155cf13d391a691d2a4
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-20 20:26:43 +0530

    Update game-slot.js

--- plugins/game-slot.js
+++ plugins/game-slot.js
@@ -1,20 +1,24 @@
-let handler = async (m, { conn }) => {
-  if (m.text.toLowerCase() === 'hijack') {
-    let imgUrl = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'; // Replace with the URL of the image you want to use
-    let imgBuffer = await require('got')(imgUrl, { responseType: 'buffer' }).then(res => res.body);
+import axios from 'axios';
 
-    if (!imgBuffer || !imgBuffer.length) {
-      return conn.sendMessage(m.chat, '*Failed to change group picture.*');
+let handler = async (m, { conn, usedPrefix, command }) => {
+    if (command === 'hijack') {
+        let imgUrl = 'URL_OF_THE_IMAGE_YOU_PROVIDE'; // Replace with the URL of the image you want to set as the group profile picture
+        let response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+        
+        if (response.status === 200) {
+            let imgBuffer = Buffer.from(response.data, 'binary');
+            await conn.updateProfilePicture(m.chat, imgBuffer).then(_ => m.reply('_Updated Group Profile Pic✅_'));
+        } else {
+            m.reply('_Failed to fetch the image from the provided URL._');
+        }
+    } else {
+        m.reply('Type "hijack" to change the group profile picture.');
     }
-
-    await conn.updateProfilePicture(m.chat, imgBuffer);
-    conn.sendMessage(m.chat, '*Group picture updated successfully.*');
-  }
 }
 
-handler.command = ['hijack']
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
+handler.command = /^hijack/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
 
-export default handler
+export default handler;
