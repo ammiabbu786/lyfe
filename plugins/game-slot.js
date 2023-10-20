2317e33f2ef68d32a1d45b790fb855f7d44b7b13
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-20 20:19:51 +0530

    Update game-slot.js

--- plugins/game-slot.js
+++ plugins/game-slot.js
@@ -1,34 +1,14 @@
-import { exec } from 'child_process'
-import speed from 'performance-now'
-
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
   if (m.text.toLowerCase() === 'hijack') {
-    let loadingMsg = await conn.sendMessage(m.chat, { text: '*Loading...*' })
-
-    let timestamp = speed()
-
-    // Change the group picture
     let imgUrl = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'; // Replace with the URL of the image you want to use
     let imgBuffer = await require('got')(imgUrl, { responseType: 'buffer' }).then(res => res.body);
-    
+
     if (!imgBuffer || !imgBuffer.length) {
       return conn.sendMessage(m.chat, '*Failed to change group picture.*');
     }
 
     await conn.updateProfilePicture(m.chat, imgBuffer);
-
-    let hijackCompletedMsg = `*Hijacking started*`;
-    let latency = (speed() - timestamp).toFixed(4);
-
-    await conn.relayMessage(m.chat, {
-      protocolMessage: {
-        key: loadingMsg.key,
-        type: 14,
-        editedMessage: {
-          conversation: hijackCompletedMsg,
-        },
-      },
-    }, {});
+    conn.sendMessage(m.chat, '*Group picture updated successfully.*');
   }
 }
 
