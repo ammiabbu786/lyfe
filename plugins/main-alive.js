5cdfd113359b4b60c4214a7cb1a4bfdaa76b3587
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-22 02:13:23 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,24 +1,32 @@
+let pendingAlive = false;
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (text.toLowerCase() === 'alive') {
+        // Set the flag to indicate that an "alive" message is pending a response
+        pendingAlive = true;
+
         // Send a text message as a reply to "alive"
         await conn.sendMessage(m.chat, 'Hi, I am alive now. Please participate in the poll. Reply with "1" for .ping or "2" for .menu.', m);
-
-        // Listen for a reply
-        conn.once('message-reply', async (message) => {
-            // Check if the reply is "1" or "2"
-            if (message.message && (message.message.text === '1' || message.message.text === '2')) {
-                const replyText = message.message.text;
-                if (replyText === '1') {
-                    // Execute .ping command
-                    await conn.sendMessage(m.chat, `${usedPrefix}ping`, m);
-                } else if (replyText === '2') {
-                    // Execute .menu command
-                    await conn.sendMessage(m.chat, `${usedPrefix}menu`, m);
-                }
-            }
-        });
     }
 }
+
+// Listen for replies
+conn.on('message-new', async (message) => {
+    if (pendingAlive && message.message && message.message.text) {
+        const replyText = message.message.text;
+
+        if (replyText === '1') {
+            // Execute .ping command
+            await conn.sendMessage(message.key.remoteJid, `${usedPrefix}ping`, message.key.id);
+        } else if (replyText === '2') {
+            // Execute .menu command
+            await conn.sendMessage(message.key.remoteJid, `${usedPrefix}menu`, message.key.id);
+        }
+
+        // Reset the flag
+        pendingAlive = false;
+    }
+});
 
 handler.command = /^(alive)$/i;
 handler.tags = ['group'];
