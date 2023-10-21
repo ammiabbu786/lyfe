90992686f155583d5235ed93e364125f7935aa37
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-22 01:59:16 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,40 +1,23 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    // To get sender's name
-    let name = m.pushName || conn.getName(m.sender);
+    if (text.toLowerCase() === 'alive') {
+        // Create a poll with predefined settings
+        const pollMessage = {
+            name: 'Polling Request',
+            values: ['test1', 'test2'],
+            multiselect: false,
+            selectableCount: 1
+        };
 
-    // Define the content
-    let mainImg = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png";
-    let smallImg = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png";
-    let smallText = "I'M Alive Now";
-    let mainText = "𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁";
-    let audioUrl = "https://raw.githubusercontent.com/AbhishekSuresh2/ABHISHEK-SER/main/src/mp3/Abhi.mp3";
-
-    // Construct the poll message
-    let poll = {
-        title: "Poll Title",
-        options: ["Option 1", "Option 2"],
-    };
-
-    let pollMessage = conn.prepareMessage(m.chat, poll, 'poll');
-    
-    // Construct the main message
-    let con = {
-        key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) },
-        message: {
-            contactMessage: {
-                displayName: `${name}`,
-                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-            }
-        }
-    };
-
-    // Send the main message and poll
-    await conn.sendMessage(m.chat, con, { quoted: pollMessage });
-
+        // Send the poll with a message
+        await conn.sendMessage(m.chat, {
+            poll: pollMessage
+        });
+        // Send a text message along with the poll
+        await conn.sendMessage(m.chat, 'Hi, I am alive now. Please participate in the poll.', m);
+    }
 }
 
-handler.help = ['pollalive']
-handler.tags = ['main']
-handler.command = /^(pollalive)$/i
+handler.command = /^(alive)$/i;
+handler.tags = ['group'];
 
 export default handler;
