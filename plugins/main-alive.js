6a2beb43ca24715e78accce97672222acf44317a
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-21 23:43:34 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,37 +1,37 @@
-import { exec } from 'child_process'
-import speed from 'performance-now'
+import { exec } from 'child_process';
+import speed from 'performance-now';
 
 let handler = async (m, { conn }) => {
-  // Send a message to indicate that the bot is alive
-  let aliveMsg = await conn.sendMessage(m.chat, { text: '*🟢 I am alive!*' });
+  // Create the poll options
+  let pollOptions = [
+    { text: 'Option 1' },
+    { text: 'Option 2' },
+  ];
 
-  // Perform any additional tasks or checks here
-
-  // Create a poll with options
-  const pollOptions = ["Option 1", "Option 2"];
-  const pollMessage = {
+  // Create the poll message
+  let pollMessage = {
     poll: {
-      title: 'Choose an option:',
+      question: 'Choose an option:',
       options: pollOptions,
     },
   };
 
-  // Send the poll
-  await conn.sendMessage(m.chat, pollMessage, 'poll');
+  let aliveMsg = await conn.sendMessage(m.chat, pollMessage, 'poll');
 
-  // Calculate latency
   let timestamp = speed();
+
   await exec('neofetch --stdout', async (error, stdout) => {
     let latency = (speed() - timestamp).toFixed(4);
+
     await conn.relayMessage(m.chat, {
       protocolMessage: {
         key: aliveMsg.key,
         type: 14,
         editedMessage: {
-          conversation: `*🟢 I am alive! Latency: ${latency} ms*`,
+          conversation: `*🌟Alive!* ${latency} ms`,
         },
       },
-    }, {});
+    });
   });
 };
 
