9a89c56b0229bc31947da51960f7f62c5969ebd4
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-21 23:37:14 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,69 +1,42 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  // To get sender's name
-  let name = m.pushName || conn.getName(m.sender);
+import { exec } from 'child_process'
+import speed from 'performance-now'
 
-  // Define the content
-  let mainImg = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png"; // Main image URL
-  let smallImg = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png"; // Small image URL
-  let smallText = "I'M Alive Now"; // Small text
-  let mainText = "𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁"; // Main text
-  let audioUrl = "https://raw.githubusercontent.com/AbhishekSuresh2/ABHISHEK-SER/main/src/mp3/Abhi.mp3"; // Audio URL
+let handler = async (m, { conn }) => {
+  // Send a message to indicate that the bot is alive
+  let aliveMsg = await conn.sendMessage(m.chat, { text: '*🟢 I am alive!*' });
 
-  // Construct the message
-  let con = {
-    key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) },
-    message: {
-      contactMessage: {
-        displayName: `${name}`, // Replace with the desired display name
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-      }
-    }
-  };
+  // Perform any additional tasks or checks here
 
-  // Create the poll options
-  let pollOptions = ["Test1", "Test2"];
-
-  // Create the poll message
-  let pollMessage = {
+  // Create a poll with options
+  const pollOptions = ["Option 1", "Option 2"];
+  const pollMessage = {
     poll: {
-      title: "Choose an option:",
-      options: pollOptions
-    }
+      title: 'Choose an option:',
+      options: pollOptions,
+    },
   };
 
   // Send the poll
-  await conn.sendMessage(m.chat, pollMessage, sendMessage.poll, { quoted: con });
+  await conn.sendMessage(m.chat, pollMessage, 'poll');
 
-  // Create the response message
-  let doc = {
-    audio: {
-      url: audioUrl
-    },
-    mimetype: 'audio/mp4',
-    ptt: true,
-    waveform: [100, 0, 100, 0, 100, 0, 100],
-    fileName: "ABHISHEK-SER",
-    contextInfo: {
-      mentionedJid: [m.sender],
-      externalAdReply: {
-        title: smallText,
-        body: mainText,
-        thumbnailUrl: smallImg, // Small image
-        sourceUrl: 'https://github.com/AbhishekSuresh2/ABHISHEK-SER',
-        mediaType: 1,
-        renderLargerThumbnail: false,
-        mediaUrl: mainImg // Main image
-      }
-    }
-  };
-
-  // Send the "alive" message
-  await conn.sendMessage(m.chat, doc, sendMessage.text, { quoted: con });
-
-}
+  // Calculate latency
+  let timestamp = speed();
+  await exec('neofetch --stdout', async (error, stdout) => {
+    let latency = (speed() - timestamp).toFixed(4);
+    await conn.relayMessage(m.chat, {
+      protocolMessage: {
+        key: aliveMsg.key,
+        type: 14,
+        editedMessage: {
+          conversation: `*🟢 I am alive! Latency: ${latency} ms*`,
+        },
+      },
+    }, {});
+  });
+};
 
 handler.help = ['alive']
 handler.tags = ['main']
-handler.command = /^(alive)$/i
+handler.command = ['alive']
 
-export default handler;
+export default handler
