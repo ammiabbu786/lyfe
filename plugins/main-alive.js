943b184fa3493ed67507636ad5d42dc489984900
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-22 02:09:19 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,19 +1,21 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (text.toLowerCase() === 'alive') {
-        // Create a poll with predefined settings
-        const pollMessage = {
-            name: 'Polling Request',
-            values: ['test1', 'test2'],
-            multiselect: false,
-            selectableCount: 1
-        };
+        // Send a text message as a reply to "alive"
+        await conn.sendMessage(m.chat, 'Hi, I am alive now. Please participate in the poll. Reply with "1" for .ping or "2" for .menu.', m);
 
-        // Send a text message as a reply to "alive" before sending the poll
-        await conn.sendMessage(m.chat, 'Hi, I am alive now. Please participate in the poll.', m);
-        
-        // Send the poll
-        await conn.sendMessage(m.chat, {
-            poll: pollMessage
+        // Listen for a reply
+        conn.once('message-reply', async (message) => {
+            // Check if the reply is "1" or "2"
+            if (message.message && (message.message.text === '1' || message.message.text === '2')) {
+                const replyText = message.message.text;
+                if (replyText === '1') {
+                    // Execute .ping command
+                    await conn.sendMessage(m.chat, `${usedPrefix}ping`, m);
+                } else if (replyText === '2') {
+                    // Execute .menu command
+                    await conn.sendMessage(m.chat, `${usedPrefix}menu`, m);
+                }
+            }
         });
     }
 }
