d1594c7889d87bc75f423fba22e7a0c84896f2b4
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-23 00:35:04 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -824,26 +824,18 @@ global.dfail = (type, m, conn) => {
     }
 
     const msg = {
-        owner: `*${emoji.owner} Owner's Query*\n
-    ${userTag} This command can only be used by the *Bot Owner*!`,
-        moderator: `*${emoji.moderator} Moderator's Query*\n
-    ${userTag} This command can only be used by *Moderators*!`,
-        premium: `*${emoji.premium} Premium Query*\n
-    ${userTag} This command is only for *Premium Members*!`,
-        group: `*${emoji.group} Group Query*\n
-    ${userTag} This command can only be used in *Group Chats*!`,
-        private: `*${emoji.private} Private Query*\n
-    ${userTag} This command can only be used in *Private Chats*!`,
-        admin: `*${emoji.admin} Admin's Query*\n
-    ${userTag} This command is only for *Group Admins*!`,
-        botAdmin: `*${emoji.botAdmin} Bot Admin's Query*\n
-    ${userTag} Make the bot an *Admin* to use this command!`,
+        owner: `This Command Can Only Be Used By The *Bot Owner*`,
+        moderator: `This Command Can Only Be Used By The *Moderators*`,
+        premium: `This Command Can Only Be Used By The *Premium Members*!`,
+        group: `This Command Can Only Be Used In *Groups*`,
+        private: `This Command Can Only Be Used In *Private Chats*`,
+        admin: `This Command Can Only Be Used By The *Group Admins*`,
+        botAdmin: `Make Me *Admin* To Use This Command`,
         unreg: `*${emoji.unreg} Registration Query*\n
     ${userTag} Please register to use this feature by typing:\n\n*#register name.age*\n\nExample: *#register ${m.name}.18*!`,
-        nsfw: `*${emoji.nsfw} NSFW Query*\n
-    ${userTag} NSFW is not active. Please contact the Group admin to enable this feature!`,
+        nsfw: `*NSFW* Is Not Enabled In This Group Enable It By Typing .on nsfw`,
         restrict: `*${emoji.restrict} Inactive Feature Query*\n
-    ${userTag} This feature is *disabled*!`,
+    ${userTag} This Feature Is *Disabled* In This Group`,
     }
      [type]
     if (msg) return  m.reply(msg)
