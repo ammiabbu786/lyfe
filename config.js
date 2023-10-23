535596981a32fffc6a2c64bb8b0fd7a21bc892fe
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-23 11:16:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
 
+SESSION_ID: process.env.SESSION_ID || 'Abhi_CavityBrush', //Enter Your Session Id
 
 global.owner = [
   ['919074692450', 'ABHISHEK-SURESH', true],
