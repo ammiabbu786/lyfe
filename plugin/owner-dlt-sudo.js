d40bca34a3b995537300e217dba0d31c99100a92
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-28 08:43:45 +0530

    Update owner-dlt-sudo.js

--- plugin/owner-dlt-sudo.js
+++ plugin/owner-dlt-sudo.js
@@ -2,43 +2,36 @@ import { exec } from 'child_process';
 import speed from 'performance-now';
 
 let handler = async (m, { conn }) => {
-  let animationMsg = await conn.sendMessage(m.chat, { text: '*Loading...*' });
+  let heartMsg = await conn.sendMessage(m.chat, { text: '*Loading...*' });
 
-  let animations = [
-    '(\\_/)\n( •.•)\n/>🤍',
-    '(\\_/)\n( •.•)\n/>❤️',
-    '(\\_/)\n( •.•)\n/>💚',
-    '(\\_/)\n( •.•)\n/>🧡',
-    '(\\_/)\n( •.•)\n/>💜',
-    '(\\_/)\n( •.•)\n/>💙',
-  ];
+  let emojis = ["(\\_/)\n( •.•)\n/>❤️", "(\\_/)\n( •.•)\n/>🧡", "(\\_/)\n( •.•)\n/>💛", "(\\_/)\n( •.•)\n/>💖", "(\\_/)\n( •.•)\n/>💚", "(\\_/)\n( •.•)\n/>💓", "(\\_/)\n( •.•)\n/>💙", "(\\_/)\n( •.•)\n/>💜", "(\\_/)\n( •.•)\n/>💝", "(\\_/)\n( •.•)\n/>🖤", "(\\_/)\n( •.•)\n/>🤎", "(\\_/)\n( •.•)\n/>💕", "(\\_/)\n( •.•)\n/>🤍", "(\\_/)\n( •.•)\n/>💗", "_Made By ©ABHISHEK-SER_"];
+  let delay = 1000; // Delay in milliseconds between editing emojis
 
-  let delay = 1000; // Delay in milliseconds between editing animations
   let timestamp = speed();
 
   await exec('neofetch --stdout', async (error, stdout) => {
     let latency = (speed() - timestamp).toFixed(4);
 
-    for (let animation of animations) {
+    for (let emoji of emojis) {
       setTimeout(async () => {
         await conn.relayMessage(m.chat, {
           protocolMessage: {
-            key: animationMsg.key,
+            key: heartMsg.key,
             type: 14,
             editedMessage: {
-              conversation: animation,
+              conversation: emoji,
             },
           },
         }, {});
       }, delay);
 
-      delay += 1000; // Increase the delay for the next animation
+      delay += 1000; // Increase the delay for the next emoji
     }
   });
 };
 
-handler.help = ['animations'];
+handler.help = ['test'];
 handler.tags = ['fun'];
-handler.command = ['test'];
+handler.command = ['test', 'test'];
 
 export default handler;
