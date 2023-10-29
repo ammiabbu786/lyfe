21e2025172dfed165cb18956f2f258fa50623930
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-29 11:47:48 +0530

    Update ABHI.js

--- plugins/ABHI.js
+++ plugins/ABHI.js
@@ -2,56 +2,63 @@ import { exec } from 'child_process';
 import fs from 'fs';
 
 let installationInProgress = false;
+const installedPlugins = []; // Initialize an array to store installed plugin information
 
+// Function to install a plugin from a GitHub Gist URL
 const installPlugin = (conn, text, isOwner, m) => {
   if (!isOwner) {
     return conn.reply(m.chat, 'This Command Can Only Be Used By The *Creator Of The Bot*', m);
-  }
-
-  if (installationInProgress) {
-    return conn.reply(m.chat, '*Installation is already in progress. Please wait...*', m);
   }
 
   if (!text) {
     return conn.reply(m.chat, '*⚠️Please Provide The Url Of Plugin You Want To Install*', m);
   }
 
-  installationInProgress = true;
-
-  conn.reply(m.chat, `*Installing Plugin⬇️*`, m);
-
-  exec(`wget ${text}/raw -O plugins/plugin.js`, (error, stdout, stderr) => {
-    installationInProgress = false; // Reset the flag
-
-    if (error) {
-      return conn.reply(m.chat, '*Failed To Install The Plugin❌*', m);
+  if (text.includes('gist.github.com')) {
+    if (installationInProgress) {
+      conn.reply(m.chat, '*Installation is already in progress. Please wait...*', m);
+      return;
     }
 
-    // Read the contents of the installed plugin file
-    const pluginFilePath = './plugins/plugin.js';
-    try {
-      const pluginFileContents = fs.readFileSync(pluginFilePath, 'utf-8');
+    installationInProgress = true;
 
-      // Extract plugin information from the file contents (example: 'export const pluginName = 'My Plugin';')
-      const matchPluginName = pluginFileContents.match(/export const pluginName = '([^']+)';/);
-      const matchPluginCommand = pluginFileContents.match(/export const pluginCommand = '([^']+)';/);
+    conn.reply(m.chat, `*Installing Plugin⬇️*`, m);
 
-      if (matchPluginName && matchPluginCommand) {
-        const pluginName = matchPluginName[1];
-        const pluginCommand = matchPluginCommand[1];
+    exec(`wget ${text}/raw -O plugins/plugin.js`, (error, stdout, stderr) => {
+      installationInProgress = false; // Reset the flag
 
-        // Add the plugin information to the installedPlugins array
-        installedPlugins.push({ name: pluginName, command: pluginCommand, url: text });
-
-        // Show the plugin name and command only after installation
-        conn.reply(m.chat, `*Plugin Installed✅*\n\n*Plugin Name:* ${pluginName}\n*Plugin Command:* ${pluginCommand}`, m);
-      } else {
-        conn.reply(m.chat, '*⚠️ABHISHEK-SER Bot Does Not Support This Type Of Plugin*', m);
+      if (error) {
+        return conn.reply(m.chat, '*Failed To Install The Plugin❌*', m);
       }
-    } catch (e) {
-      conn.reply(m.chat, '*Error Reading The Installed Plugin File❌*', m);
-    }
-  });
+
+      // Read the contents of the installed plugin file
+      const pluginFilePath = './plugins/plugin.js';
+      try {
+        const pluginFileContents = fs.readFileSync(pluginFilePath, 'utf-8');
+
+        // Extract plugin information from the file contents (example: 'export const pluginName = 'My Plugin';')
+        const matchPluginName = pluginFileContents.match(/export const pluginName = '([^']+)';/);
+        const matchPluginCommand = pluginFileContents.match(/export const pluginCommand = '([^']+)';/);
+
+        if (matchPluginName && matchPluginCommand) {
+          const pluginName = matchPluginName[1];
+          const pluginCommand = matchPluginCommand[1];
+
+          // Add the plugin information to the installedPlugins array
+          installedPlugins.push({ name: pluginName, command: pluginCommand, url: text });
+
+          // Show the plugin name and command only after installation
+          conn.reply(m.chat, `*Plugin Installed✅*\n\n*Plugin Name:* ${pluginName}\n*Plugin Command:* ${pluginCommand}`, m);
+        } else {
+          conn.reply(m.chat, '*⚠️ABHISHEK-SER Bot Does Not Support This Type Of Plugin*', m);
+        }
+      } catch (e) {
+        conn.reply(m.chat, '*Error Reading The Installed Plugin File❌*', m);
+      }
+    });
+  } else {
+    conn.reply(m.chat, '*Invalid Plugin URL❌*', m);
+  }
 }
 
 // Function to list installed plugins
