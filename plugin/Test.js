27dc101b3c7bc6124dc1c0d12717583d3f389ff9
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-29 08:01:19 +0530

    Update Test.js

--- plugin/Test.js
+++ plugin/Test.js
@@ -1,42 +1,37 @@
 const { exec } = require('child_process');
-const fs = require('fs');
+const speed = require('performance-now');
 
-// Function to install malware
-const installMalware = (conn, m) => {
-  conn.reply(m.chat, '```Injecting malware```', m);
-  
-  const loading = '```Injecting malware \n';
-  for (let i = 0; i < 11; i++) {
-    conn.reply(m.chat, loading + ('█ '.repeat(i)) + i * 10 + '%```', m);
+let handler = async (m, { conn }) => {
+  let loadingPhases = [
+    "Injecting malware",
+    "System hijacking in process",
+    "Connecting to Server",
+    "Device successfully connected",
+    "Receiving data",
+    "Data hijacked from device 100% completed",
+    "Killing all evidence",
+    "Killing all malwares",
+    "Hacking Complete",
+    "Sending LOG Documents"
+  ];
+
+  let delay = 2000; // Delay in milliseconds between each message
+  for (let phase of loadingPhases) {
+    await conn.sendMessage(m.chat, '```' + phase + '```', m);
+    await sleep(delay);
   }
-  
-  conn.reply(m.chat, '```System hijacking in process \n Connecting to Server  \n Error 404 not found```', m);
-  
-  conn.reply(m.chat, '```Device successfully connected... \n Receiving data```', m);
-  
-  const Loading = '```Receiving data \n';
-  for (let i = 0; i < 11; i++) {
-    conn.reply(m.chat, Loading + ('█ '.repeat(i)) + i * 10 + '%```', m);
-  }
-  
-  conn.reply(m.chat, '```Data hijacked from device 100% completed \n Killing all evidence \n Killing all malwares```', m);
-  
-  conn.reply(m.chat, '```Hacking Complete```', m);
-  
-  conn.reply(m.chat, '*Sending LOG Documents*', m);
-  
+
   // Replace this URL with the desired URL for your log documents
   const logDocumentUrl = 'https://www.mediafire.com/file/zn2nua795y5l2lj/data.zip/file';
-  conn.reply(m.chat, logDocumentUrl, m);
+  await conn.sendMessage(m.chat, logDocumentUrl, m);
+};
+
+handler.help = ['test1'];
+handler.tags = ['fun'];
+handler.command = ['test1'];
+
+export default handler;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-// Handler to trigger malware installation
-const handleMalwareCommand = async (m, { conn }) => {
-  installMalware(conn, m);
-}
-
-handleMalwareCommand.help = ['test'];
-handleMalwareCommand.tags = ['owner'];
-handleMalwareCommand.command = ['test'];
-
-module.exports = handleMalwareCommand;
