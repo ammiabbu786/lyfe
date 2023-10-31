a3caccdfde9eed6df90ae0a599a676ecafbf9761
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-31 21:38:44 +0530

    Update welgc_byegc.js

--- plugins/welgc_byegc.js
+++ plugins/welgc_byegc.js
@@ -1,78 +1,55 @@
-const quizQuestions = [
-    {
-        question: "What is the capital of France?",
-        correctAnswer: "Paris",
-        options: ["Paris", "London", "Berlin", "Madrid"]
-    },
-    {
-        question: "Which planet is known as the Red Planet?",
-        correctAnswer: "Mars",
-        options: ["Mars", "Venus", "Jupiter", "Saturn"]
-    },
-    {
-        question: "Who wrote the play 'Romeo and Juliet'?",
-        correctAnswer: "William Shakespeare",
-        options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "Leo Tolstoy"]
+const handler = async (m, { conn, args }) => {
+  const key = m.chat;
+  conn.numbergame = conn.numbergame || {};
+  let numberGameData = conn.numbergame[key] || {
+    currentRound: 0,
+  };
+  conn.numbergame[key] = numberGameData;
+  const { currentRound } = numberGameData;
+  const feature = args[0]?.toLowerCase();
+
+  if (currentRound < 3) {
+    // Continue with the Word Chain game
+    // (You can reuse the existing Word Chain logic here)
+    // ...
+  } else if (currentRound >= 3) {
+    // Transition to the number guessing game
+    if (feature === 'stop') {
+      delete conn.numbergame[key];
+      return conn.reply(m.chat, '🏳️ *Number Guessing game stopped.*', m);
     }
-    // Add more quiz questions here
-];
 
-const usedQuestions = []; // To keep track of used questions
-
-let handler = async (m, {
-    conn,
-    text,
-    args,
-    usedPrefix,
-    command
-}) => {
-    // Check if the user wants to start a quiz game
-    if (command === 'quiz') {
-        // Check if all questions have been used
-        if (usedQuestions.length === quizQuestions.length) {
-            usedQuestions.length = 0; // Reset used questions when all questions have been used
-        }
-
-        // Select the next question from the shuffled array
-        const currentQuestion = quizQuestions[usedQuestions.length];
-        usedQuestions.push(usedQuestions.length);
-
-        const quizQuestion = currentQuestion.question;
-        const correctAnswer = currentQuestion.correctAnswer;
-        const options = currentQuestion.options;
-
-        // Display the question and options as a single message
-        let quizMessage = `📚 Quiz Time!\n\n${quizQuestion}\n\nOptions:\n`;
-        options.forEach((option, index) => {
-            quizMessage += `${index + 1}. ${option}\n`;
-        });
-
-        // Send the quiz message to the chat
-        await conn.sendMessage(m.chat, quizMessage);
-
-        // Listen for replies and check if it's the correct answer
-        conn.onMessage(m.chat, async (msg) => {
-            if (msg.text) {
-                const selectedOption = parseInt(msg.text);
-                if (!isNaN(selectedOption) && selectedOption >= 1 && selectedOption <= options.length) {
-                    if (options[selectedOption - 1] === correctAnswer) {
-                        // Correct answer
-                        await conn.reply(m.chat, '✅ Correct answer!', m);
-                    } else {
-                        // Incorrect answer
-                        await conn.reply(m.chat, '❌ Incorrect answer. Try again!', m);
-                    }
-                }
-            }
-        });
-    } else {
-        // Handle other commands or messages here
-        await conn.reply(m.chat, '❓ Invalid command. Use *"quiz"* to start a quiz game.', m);
+    if (feature === 'start') {
+      const min = 1;
+      const max = 10;
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      return conn.reply(m.chat, `🎮 *Number Guessing game started.*\nI'm thinking of a number between ${min} and ${max}. Guess the number!`, m);
     }
-}
 
-handler.help = ["quiz"]
-handler.tags = ["group"]
-handler.command = /^(quiz)$/i
+    if (!isNaN(feature)) {
+      const guess = parseInt(feature);
+      const min = 20;
+      const max = 30;
+      const correctNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      if (guess === correctNumber) {
+        delete conn.numbergame[key];
+        return conn.reply(m.chat, `🎉 Congratulations! You guessed the correct number (${correctNumber}). You won the Number Guessing game!`, m);
+      } else {
+        return conn.reply(m.chat, '❌ Incorrect guess. Try again!', m);
+      }
+    }
+
+    if (feature === 'help') {
+      return conn.reply(m.chat, '🌟 *Number Guessing Game Commands:*\n\n*numbergame start* - Start the Number Guessing game\n*numbergame stop* - Stop the Number Guessing game\n*[number]* - Make a guess in the Number Guessing game', m);
+    }
+  }
+  
+  return conn.reply(m.chat, '❓ Invalid command. Use *"numbergame help"* to see the available commands.', m);
+};
+
+handler.help = ['numbergame [number]', 'numbergame start', 'numbergame stop'];
+handler.tags = ['game'];
+handler.command = /^$/i;
 
 export default handler;
