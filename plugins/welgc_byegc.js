4b3a3a754d1fd1d7cdbecde5f5a24bdb3e46e210
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-10-31 22:24:33 +0530

    Update welgc_byegc.js

--- plugins/welgc_byegc.js
+++ plugins/welgc_byegc.js
@@ -2,19 +2,20 @@ const handler = async (m, { conn, args }) => {
   const key = m.chat;
   conn.quizgame = conn.quizgame || {};
   let quizGameData = conn.quizgame[key] || {
-    currentQuestion: 0,
+    currentQuestion: -1,
     questions: [
       { question: 'What is the capital of France?', answer: 'Paris' },
       { question: 'How many continents are there on Earth?', answer: '7' },
       { question: 'What is the largest planet in our solar system?', answer: 'Jupiter' },
     ],
   };
+
   conn.quizgame[key] = quizGameData;
   const { currentQuestion, questions } = quizGameData;
 
   if (args[0] === 'start') {
-    conn.quizgame[key].currentQuestion = 0;
-    return askQuestion(conn, m.chat, key);
+    conn.quizgame[key].currentQuestion = -1;
+    return askRandomQuestion(conn, m.chat, key);
   }
 
   if (currentQuestion < questions.length) {
@@ -23,27 +24,33 @@ const handler = async (m, { conn, args }) => {
       const correctAnswer = questions[currentQuestion].answer.toLowerCase();
       if (userAnswer.toLowerCase() === correctAnswer) {
         conn.quizgame[key].currentQuestion++;
-        if (currentQuestion < questions.length) {
-          return askQuestion(conn, m.chat, key);
+        if (currentQuestion < questions.length - 1) {
+          return askRandomQuestion(conn, m.chat, key);
+        } else {
+          delete conn.quizgame[key];
+          return conn.reply(m.chat, '🎉 Congratulations! You completed the Quiz game!', m);
         }
+      } else {
+        return conn.reply(m.chat, '❌ Incorrect answer. Try again!', m);
       }
+    } else {
+      return conn.reply(m.chat, '❓ Please provide an answer to the current question.', m);
     }
+  } else {
+    return conn.reply(m.chat, '🏁 The Quiz game is already completed. Use *"start"* to begin a new quiz game.', m);
   }
-
-  delete conn.quizgame[key];
-  return conn.reply(m.chat, '🏁 The Quiz game is completed. Use *"start"* to begin a new quiz game.', m);
 };
 
-function askQuestion(conn, chatId, key) {
-  const { currentQuestion, questions } = conn.quizgame[key];
-  if (currentQuestion < questions.length) {
-    const question = questions[currentQuestion].question;
-    return conn.reply(chatId, `📝 Question ${currentQuestion + 1}: ${question}`, null, {
-      contextInfo: {
-        mentionedJid: [conn.user.jid],
-      },
-    });
-  }
+function askRandomQuestion(conn, chatId, key) {
+  const { questions } = conn.quizgame[key];
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  const question = questions[randomIndex].question;
+  conn.quizgame[key].currentQuestion = randomIndex;
+  return conn.reply(chatId, `📝 Question: ${question}`, null, {
+    contextInfo: {
+      mentionedJid: [conn.user.jid],
+    },
+  });
 }
 
 handler.help = ['start', '[answer]'];
