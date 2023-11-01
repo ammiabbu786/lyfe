9000810d46a31d52b9f1e0cb4cd952a016a44beb
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-11-01 08:00:17 +0530

    Update welgc_byegc.js

--- plugins/welgc_byegc.js
+++ plugins/welgc_byegc.js
@@ -3,10 +3,12 @@ const handler = async (m, { conn, args }) => {
   conn.numbergame = conn.numbergame || {};
   let numberGameData = conn.numbergame[key] || {
     numberToGuess: 0,
+    players: [],
+    currentPlayer: null,
     attempts: 0,
   };
   conn.numbergame[key] = numberGameData;
-  const { numberToGuess, attempts } = numberGameData;
+  const { numberToGuess, players, currentPlayer, attempts } = numberGameData;
   const guess = args[0];
 
   if (args[0] === 'start') {
@@ -14,21 +16,48 @@ const handler = async (m, { conn, args }) => {
       return conn.reply(m.chat, '⚠️ *Game already in progress.*', m);
     }
     numberGameData.numberToGuess = getRandomNumber(); // Implement a function to get a random number
+    numberGameData.players = [];
+    numberGameData.currentPlayer = null;
     numberGameData.attempts = 0;
-    return conn.reply(m.chat, '🎮 *Number Guessing Game started.*\nTry to guess the number!', m);
+    return conn.reply(m.chat, '🎮 *Number Guessing Game started.*\nPlayers can join using *join* command.\nUse *startgame* to begin.', m);
   }
 
-  if (numberToGuess !== 0 && guess !== undefined) {
+  if (args[0] === 'join') {
+    const senderId = m.sender;
+    if (!players.includes(senderId)) {
+      players.push(senderId);
+      return conn.reply(m.chat, `🙌 @${senderId.split('@')[0]} joined the game!`, m, { mentions: [senderId] });
+    } else {
+      return conn.reply(m.chat, '🙅‍♂️ *You have already joined this game.*', m);
+    }
+  }
+
+  if (args[0] === 'startgame') {
+    if (players.length < 2) {
+      return conn.reply(m.chat, '⚠️ *At least two players are required to start the game.*', m);
+    }
+    if (numberToGuess !== 0) {
+      return conn.reply(m.chat, '⚠️ *Game already in progress.*', m);
+    }
+    numberGameData.currentPlayer = players[0];
+    return conn.reply(m.chat, `🏁 Game has started! It's @${currentPlayer.split('@')[0]}'s turn to guess.`, m);
+  }
+
+  if (numberToGuess !== 0 && currentPlayer === m.sender && guess !== undefined) {
     const playerGuess = parseInt(guess);
     if (!isNaN(playerGuess)) {
       numberGameData.attempts++;
       if (playerGuess === numberToGuess) {
         delete conn.numbergame[key];
-        return conn.reply(m.chat, `🎉 Congratulations! You guessed the number ${numberToGuess} in ${attempts} attempts!`, m);
+        return conn.reply(m.chat, `🎉 @${currentPlayer.split('@')[0]} guessed the number ${numberToGuess} in ${attempts} attempts!`, m);
       } else if (playerGuess < numberToGuess) {
-        return conn.reply(m.chat, '🔽 Try a higher number.', m);
+        const nextPlayerIndex = (players.indexOf(currentPlayer) + 1) % players.length;
+        numberGameData.currentPlayer = players[nextPlayerIndex];
+        return conn.reply(m.chat, `🔽 @${currentPlayer.split('@')[0]}, try a higher number. It's @${currentPlayer.split('@')[0]}'s turn.`, m);
       } else {
-        return conn.reply(m.chat, '🔼 Try a lower number.', m);
+        const nextPlayerIndex = (players.indexOf(currentPlayer) + 1) % players.length;
+        numberGameData.currentPlayer = players[nextPlayerIndex];
+        return conn.reply(m.chat, `🔼 @${currentPlayer.split('@')[0]}, try a lower number. It's @${currentPlayer.split('@')[0]}'s turn.`, m);
       }
     } else {
       return conn.reply(m.chat, '❌ Please enter a valid number.', m);
@@ -43,6 +72,8 @@ const handler = async (m, { conn, args }) => {
   if (args[0] === 'help') {
     return conn.reply(m.chat, '🌟 *Number Guessing Game Commands:*\n\n' +
       'Type *start* to begin a new game.\n' +
+      'Type *join* to join the game.\n' +
+      'Type *startgame* to start the game (requires at least two players).\n' +
       'Type a number to make a guess.\n' +
       'Type *stop* to end the game.', m);
   }
@@ -57,7 +88,7 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
 }
 
-handler.help = ['numbergame [number]', 'numbergame start', 'numbergame stop'];
+handler.help = ['numbergame [number]', 'numbergame start', 'numbergame join', 'numbergame startgame', 'numbergame stop'];
 handler.tags = ['game'];
 handler.command = /^numbergame$/i;
 
