eda35c1ffdc59634593ef58c21c8c6dee3086faf
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-11-01 13:42:57 +0530

    Update welgc_byegc.js

--- plugins/welgc_byegc.js
+++ plugins/welgc_byegc.js
@@ -1,140 +1,49 @@
-const handler = async (m, { conn, args }) => {
-  const key = m.chat;
-  conn.numbergame = conn.numbergame || {};
-  let numberGameData = conn.numbergame[key] || {
-    round: 0,
-    numberToGuess: 0,
-    players: [],
-    currentPlayer: null,
-    attempts: 0,
-    gameStartTimeout: null, // Timeout for automatic game start
-  };
-  conn.numbergame[key] = numberGameData;
-  const { round, numberToGuess, players, currentPlayer, attempts, gameStartTimeout } = numberGameData;
-  const guess = args[0];
+// Modify the function to set background music (BGM)
+const setBGM = (conn, text, m) => {
+  const audioUrl = text.trim(); // Assume text contains the audio link
+  // Validate the audio link here, e.g., check if it's an MP3 or MP4 link
 
-  const roundRanges = [
-    { min: 1, max: 10 },
-    { min: 30, max: 50 },
-    { min: 60, max: 80 },
-    // Define more ranges for rounds 4, 5, and 6 as needed
-  ];
+  // Store the BGM URL for later use (you can use a global variable or a database)
+  const bgmUrl = audioUrl;
 
-  if (args[0] === 'start') {
-    if (round >= 6) {
-        return conn.reply(m.chat, '🏁 *All rounds have been completed.*', m);
-    }
+  // You can set the BGM URL in your bot's settings or a database for future use
+  // Example: botSettings.bgmUrl = bgmUrl;
 
-    // Check if a game is in progress
-    if (numberToGuess !== 0) {
-        return conn.reply(m.chat, '⚠️ *Game already in progress.*', m);
-    }
-
-    // Check if the game start timeout has occurred
-    if (gameStartTimeout !== null) {
-        clearTimeout(gameStartTimeout);
-        conn.reply(m.chat, '⚠️ *Game start timeout occurred.* Starting a new game.', m);
-    } else {
-        conn.reply(m.chat, '⚠️ Not enough players joined. Starting a new game.', m);
-    }
-
-    // Start a new game
-    numberGameData.numberToGuess = getRandomNumber(roundRanges[round].min, roundRanges[round].max);
-    numberGameData.players = [];
-    numberGameData.currentPlayer = null;
-    numberGameData.attempts = 0;
-
-    // Start a 15-second timer for automatic game start
-    numberGameData.gameStartTimeout = setTimeout(() => {
-        if (players.length >= 2) {
-            numberGameData.currentPlayer = players[0];
-            conn.reply(m.chat, `🏁 Round ${round + 1} has started! Guess a number between ${roundRanges[round].min}-${roundRanges[round].max}. It's @${currentPlayer.split('@')[0]}'s turn to guess.`, m);
-        } else {
-            conn.reply(m.chat, '⚠️ Not enough players joined. Starting a new game.', m);
-        }
-    }, 15000);
-
-    return conn.reply(m.chat, `🎮 *Number Guessing Game - Round ${round + 1} started.*\nPlayers can join using *join* command.\nAutomatic game start in 15 seconds.\nUse *startgame* to begin manually.`, m);
-}
-  
-
-  if (args[0] === 'join') {
-    const senderId = m.sender;
-    if (!players.includes(senderId)) {
-      players.push(senderId);
-      return conn.reply(m.chat, '🙌 @' + senderId.split('@')[0] + ' joined the game!', m);
-    } else {
-      return conn.reply(m.chat, '🙅‍♂️ *You have already joined this game.*', m);
-    }
-  }
-
-  if (args[0] === 'startgame') {
-    if (players.length < 2) {
-      return conn.reply(m.chat, '⚠️ *At least two players are required to start the game.*', m);
-    }
-    if (numberToGuess !== 0) {
-      return conn.reply(m.chat, '⚠️ *Game already in progress.*', m);
-    }
-    numberGameData.currentPlayer = players[0];
-    clearTimeout(gameStartTimeout); // Clear the game start timeout
-    return conn.reply(m.chat, `🏁 Round ${round + 1} has started! Guess a number between ${roundRanges[round].min}-${roundRanges[round].max}. It's @${currentPlayer.split('@')[0]}'s turn to guess.`, m);
-  }
-
-  if (numberToGuess !== 0 && currentPlayer === m.sender && guess !== undefined) {
-    const playerGuess = parseInt(guess);
-    if (!isNaN(playerGuess)) {
-      numberGameData.attempts++;
-      if (playerGuess === numberToGuess) {
-        if (round >= 5) {
-          delete conn.numbergame[key];
-          return conn.reply(m.chat, '🎉 @' + currentPlayer.split('@')[0] + ` guessed the number ${numberToGuess} in ${attempts} attempts! All rounds have been completed.`, m);
-        } else {
-          numberGameData.round++;
-          numberGameData.numberToGuess = 0;
-          conn.reply(m.chat, `🎉 @${currentPlayer.split('@')[0]} guessed the number ${numberToGuess} in ${attempts} attempts! Round ${round + 1} completed.`, m);
-          return conn.reply(m.chat, '🏁 *Next round is starting...*', m);
-        }
-      } else if (playerGuess < numberToGuess) {
-        const nextPlayerIndex = (players.indexOf(currentPlayer) + 1) % players.length;
-        numberGameData.currentPlayer = players[nextPlayerIndex];
-        return conn.reply(m.chat, `🔽 @${currentPlayer.split('@')[0]}, try a higher number. It's @${currentPlayer.split('@')[0]}'s turn.`, m);
-      } else {
-        const nextPlayerIndex = (players.indexOf(currentPlayer) + 1) % players.length;
-        numberGameData.currentPlayer = players[nextPlayerIndex];
-        return conn.reply(m.chat, `🔼 @${currentPlayer.split('@')[0]}, try a lower number. It's @${currentPlayer.split('@')[0]}'s turn.`, m);
-      }
-    } else {
-      return conn.reply(m.chat, '❌ Please enter a valid number.', m);
-    }
-  }
-
-  if (args[0] === 'stop') {
-    delete conn.numbergame[key];
-    clearTimeout(gameStartTimeout); // Clear the game start timeout
-    return conn.reply(m.chat, '🏳️ *Number Guessing Game stopped.*', m);
-  }
-
-  if (args[0] === 'help') {
-    return conn.reply(m.chat, '🌟 *Number Guessing Game Commands:*\n\n' +
-      'Type *start* to begin a new game.\n' +
-      'Type *join* to join the game.\n' +
-      'Type *startgame* to start the game (requires at least two players).\n' +
-      'Type a number to make a guess.\n' +
-      'Type *stop* to end the game.', m);
-  }
-
-  return conn.reply(m.chat, '❓ Invalid command. Use *"help"* to see the available commands.', m);
+  conn.reply(m.chat, `Background music has been set to the provided link.`, m);
 };
 
-// Implement the getRandomNumber function as needed.
+// Modify the handler to handle setting BGM
+const handlePluginCommand = async (m, { conn, text, isOwner }) => {
+  if (text.startsWith('setbgm')) {
+    const bgmLink = text.replace('setbgm', '').trim();
+    setBGM(conn, bgmLink, m);
+  } else if (text === 'get') {
+    // If the user enters "plugin get", show the specified text
+    conn.reply(m.chat, 'hi plugins hi link hi', m);
+  } else if (text.startsWith('list')) {
+    listInstalledPlugins(conn, m);
+  } else if (text.startsWith('remove')) {
+    const name = text.replace('remove', '').trim();
+    if (name) {
+      removePluginByName(conn, name, m);
+    } else {
+      conn.reply(m.chat, 'Please provide the name of the plugin you want to remove.', m);
+    }
+  } else {
+    installPlugin(conn, text, isOwner, m);
+  }
+};
 
-// Function to get a random number within the specified range
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// Update the help and tags for the modified command
+handlePluginCommand.help = [
+  'plugin <url> - Install a plugin from the given URL.',
+  'setbgm <audio_link> - Set background music using an audio link.',
+  'plugin list - List installed plugins.',
+  'plugin remove <name> - Remove an installed plugin by name.'
+];
+handlePluginCommand.tags = ['owner'];
 
-handler.help = ['numbergame [number]', 'numbergame start', 'numbergame join', 'numbergame startgame', 'numbergame stop'];
-handler.tags = ['game'];
-handler.command = /^numbergame$/i;
+// Command names can remain the same
+handlePluginCommand.command = ['plugin', 'setbgm'];
 
-export default handler;
+export default handlePluginCommand;
