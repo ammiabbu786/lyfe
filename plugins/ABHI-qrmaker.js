fee1c4a0942a56f0c3d0eaadc0614b062ce70f5c
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-11-02 12:12:26 +0530

    Update ABHI-qrmaker.js

--- plugins/ABHI-qrmaker.js
+++ plugins/ABHI-qrmaker.js
@@ -1,9 +1,148 @@
-import { toDataURL } from 'qrcode'
-let handler = async (m, { text, conn }) => {
-if (!text) throw `*🎯 Give A Text To Convert*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*✅ Here Is Your Qr Code*', m)
+const handler = async (m, { conn, args }) => {
+  const key = m.chat;
+  conn.wordchain = conn.wordchain || {};
+  let wordchainData = conn.wordchain[key] || {
+    currentWord: null,
+    players: [],
+    hasJoined: [],
+  };
+  conn.wordchain[key] = wordchainData;
+  const { currentWord, players, hasJoined } = wordchainData;
+  const feature = args[0]?.toLowerCase();
+
+  if (feature === 'delete') {
+    delete conn.wordchain[key];
+    return conn.reply(m.chat, '🏳️ *Word Chain game stopped.*', m);
+  }
+
+  if (feature === 'create') {
+    if (currentWord) {
+      return conn.reply(m.chat, '⚠️ *Game already in progress.*', m);
+    }
+
+    // Start the game with a delay of 60 seconds
+    await new Promise(resolve => setTimeout(resolve, 60000));
+    
+    wordchainData.currentWord = getRandomWord(); // Implement a function to get a random word
+
+    // Provide instructions after starting the game
+    const instructions = `
+🎮 *Word Chain game started.*
+*🕒 Game starts in 60 seconds.*
+*type* \`join\` *to join the game*
+*wcg delete* *to delete this game session*
+    `;
+    
+    conn.reply(m.chat, instructions, m);
+
+    return conn.reply(
+      m.chat,
+      `Current Word: ${currentWord}\nWaiting for other players to join.`,
+      m
+    );
+  }
+
+  if (feature === 'join') {
+    const senderId = m.sender;
+    if (players.includes(senderId)) {
+      return conn.reply(m.chat, '🙅‍♂️ *You have already joined this game.*', m);
+    }
+    if (!currentWord) {
+      return conn.reply(m.chat, '⚠️ *No Word Chain game is currently waiting for players.*', m);
+    }
+    players.push(senderId);
+    hasJoined.push(senderId);
+    return conn.reply(
+      m.chat,
+      `🙌 *Players who have joined:*\n${hasJoined.map((playerId) => `- @${playerId.split('@')[0]}`).join('\n')}`,
+      m,
+      { mentions: hasJoined }
+    );
+  }
+
+  if (args[0]) {
+    const senderId = m.sender;
+    if (!currentWord) {
+      return conn.reply(m.chat, '⚠️ *The Word Chain game has not started yet.*', m);
+    }
+    const lastWord = currentWord.toLowerCase();
+    const playerWord = args[0].toLowerCase();
+    if (playerWord[0] !== lastWord[lastWord.length - 1]) {
+      return conn.reply(m.chat, '❌ *Invalid word. It must start with the last letter of the previous word.*', m);
+    }
+    if (isWordUsed(playerWord)) {
+      return conn.reply(m.chat, '❌ *That word has already been used.*', m);
+    }
+    // Check if the word is valid (you can use an API or dictionary for this)
+    if (isValidWord(playerWord)) {
+      // Update the current word and check for a win condition
+      wordchainData.currentWord = playerWord;
+      hasJoined.forEach((player) => {
+        if (player === senderId) return; // Skip the current player
+        // Send the updated word and a message to the other player
+        conn.sendMessage(
+          player + '@s.whatsapp.net',
+          `🎯 *New Word:* ${playerWord}\n\n*Your turn!*\nReply with a word that starts with: *${playerWord[playerWord.length - 1].toUpperCase()}*`
+        );
+      });
+      // Check for a win condition (e.g., a word is longer than X letters)
+      if (playerWord.length >= 5) {
+        // Declare the current player as the winner
+        const winner = m.sender.split('@')[0];
+        delete conn.wordchain[key];
+        return conn.reply(m.chat, `🎉 *${winner}* won the Word Chain game!`, m);
+      }
+      return conn.reply(m.chat, `👍 *${playerWord}* is a valid word. Waiting for the next player...`, m);
+    } else {
+      return conn.reply(m.chat, '❌ *Invalid word. Please enter a valid English word.*', m);
+    }
+  }
+
+  if (feature === 'help') {
+    return conn.reply(
+      m.chat,
+      `
+      🌟 *Word Chain Game Commands:*
+
+*wordchain create* - Start a Word Chain game
+*wordchain join* - Join a waiting Word Chain game
+*wordchain delete* - Stop the Word Chain game
+*[word]* - Play a word in the Word Chain game
+
+*Example:*
+Type *wordchain create* to start a Word Chain game.
+Type *wordchain join* to join a waiting Word Chain game.
+Type a word to continue the Word Chain game.
+    `,
+      m
+    );
+  }
+  return conn.reply(m.chat, '❓ Invalid command. Use *"wcg help"* to see the available commands.', m);
+};
+
+// Implement the getRandomWord, isWordUsed, and isValidWord functions as needed.
+
+// Placeholder function for getting a random word
+function getRandomWord() {
+  // Implement logic to generate a random word
+  // For example, you can fetch a word from an API or use a predefined list of words
+  return "apple"; // Replace with actual implementation
 }
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
-handler.tags = ['tools']
-handler.command = /^qr(code)?$/i
-export default handler
+
+// Placeholder function to check if a word has been used
+function isWordUsed(word) {
+  // Implement logic to check if the word has been used in the game
+  return false; // Replace with actual implementation
+}
+
+// Placeholder function to check if a word is valid
+function isValidWord(word) {
+  // Implement logic to check if the word is a valid English word
+  return true; // Replace with actual implementation
+}
+
+handler.help = ['wordchain [word]', 'wordchain create', 'wordchain join', 'wordchain delete'];
+handler.tags = ['game'];
+handler.command = /^wordchain$/i;
+
+export default handler;
