3298c2d6d9eafc3e057be7c327be38934d65295b
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-11-13 18:32:35 +0530

    Update and rename ABHI-nsfw to ABHI-nsfw.js

---
+++ plugins/ABHI-nsfw.js
@@ -0,0 +1,44 @@
+let handler = m => m
+handler.all = async function (m, conn) {
+    var vn = "https://raw.githubusercontent.com/AbhishekSuresh2/ABHISHEK-SER/blob/main/media/voice/test.mp3"
+    let url = "https://github.com.com/AbhishekSuresh2/ABHISHEK-SER"
+    let murl = "https://instagram.com/abhishek_ser?igshid=NGxoaTl3bTJwMmJh"
+    let hash = global.botname
+    let img = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png"
+    let num = "919074692450"
+
+    let doc = {
+        audio: {
+          url: vn
+        },
+        mimetype: 'audio/mpeg',
+        ptt: true,
+        waveform: [0,99,0,99,0,99,0],
+        fileName: "test",
+    
+        contextInfo: {
+          mentionedJid: [m.sender],
+          externalAdReply: {
+          title: "⇆ㅤ ||◁ㅤ❚❚ㅤ▷||ㅤ ⇆
+01:43 ━━━━●───── 03:50",
+          body: hash,
+          thumbnailUrl: img,
+          sourceUrl: url,
+          mediaType: 2,
+          mediaUrl: murl,
+         // renderLargerThumbnail: true,
+          showAdAttribution: true
+          }}
+      };
+	
+    let phoneNumber = '';
+    if (m.mentionedJid && m.mentionedJid[0]) {
+        phoneNumber = m.mentionedJid[0].replace(/[^0-9]/g, '');
+        if (phoneNumber === num) {
+          return this.sendMessage(m.chat, doc, { quoted: m });
+        }
+      } else {
+        return
+      }
+}
+export default handler
